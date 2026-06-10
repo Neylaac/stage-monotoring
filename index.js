265beb52081122bbeb064d2e8a1a66dff75d44ab
Ibtisam90-gif <ibtisam.madi@student.ehb.be>
2026-06-10 13:13:32 +0200

    Add stageovereenkomst overview HTML

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const path = require('path');
 const express = require('express');
 const app = express()
 
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+    res.sendFile(
+        path.join(__dirname, "views/login/stageovereenkomst-overzicht.html")
+    );
+});
 
 app.listen(port, () => {
     console.log(`App Listing on Port ${port}`);
