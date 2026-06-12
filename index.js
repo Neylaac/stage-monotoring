5e42d8bd2d5466bb18d94c96d7b2bcead27fd807
Ibtisam90-gif <ibtisam.madi@student.ehb.be>
2026-06-12 14:12:50 +0200

    Add company stage agreement workflow pages

--- index.js
+++ index.js
@@ -6,37 +6,86 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "views")));
 
-app.get("/student-stageovereenkomst-detail", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "views/html/student-stageovereenkomst-detail.html")
-    );
-});
+/* Docent */
 
 app.get("/", (req, res) => {
     res.sendFile(
-        path.join(__dirname,
-        "views/html/docent-stageovereenkomst-overzicht.html")
+        path.join(
+            __dirname,
+            "views/html/docent-stageovereenkomst-overzicht.html"
+        )
     );
 });
 
 app.get("/stageovereenkomst-detail", (req, res) => {
     res.sendFile(
-        path.join(__dirname,
-        "views/html/docent-stageovereenkomst-detail.html")
+        path.join(
+            __dirname,
+            "views/html/docent-stageovereenkomst-detail.html"
+        )
     );
 });
 
+/* Student */
+
+app.get("/student-stageovereenkomst-detail", (req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "views/html/student-stageovereenkomst-detail.html"
+        )
+    );
+});
+
+/* Itsme */
+
 app.get("/itsme", (req, res) => {
     res.sendFile(
-        path.join(__dirname, "views/html/itsme.html")
+        path.join(
+            __dirname,
+            "views/html/itsme.html"
+        )
+    );
+});
+
+/* Bedrijf */
+
+app.get("/bedrijf-stageovereenkomst-overzicht", (req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "views/html/bedrijf-stageovereenkomst-overzicht.html"
+        )
+    );
+});
+
+app.get("/bedrijf-student-overzicht", (req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "views/html/bedrijf-student-overzicht.html"
+        )
+    );
+});
+
+app.get("/bedrijf-stageovereenkomst-detail", (req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "views/html/bedrijf-stageovereenkomst-detail.html"
+        )
+    );
+});
+
+app.get("/bedrijf-stageovereenkomst-detail", (req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "views/html/bedrijf-stageovereenkomst-detail.html"
+        )
     );
 });
 
 app.listen(port, () => {
-    console.log(`App Listing on Port ${port}`);
-});
-app.get("/stageovereenkomst-detail", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "views/html/stageovereenkomst-detail.html")
-    );
+    console.log(`App Listening on Port ${port}`);
 });
