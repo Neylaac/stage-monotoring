93bad72eafead7a879c5d1b01ec98ca74b482113
Neylaac <neyla.achaoui@student.ehb.be>
2026-06-21 16:03:09 +0200

    Add student to database

--- database/testdata.sql
+++ database/testdata.sql
@@ -14,6 +14,7 @@ INSERT INTO users (voornaam, achternaam, email, wachtwoord, role) VALUES
 ('Sofie', 'Achaoui', 'sofie.achaoui@student.ehb.be', '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', 'STUDENT'),
 ('Nora', 'Achaoui', 'nora.achaoui@student.ehb.be', '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', 'STUDENT'),
 ('Ines', 'El Yanaoui', 'ines.el.yanaoui@student.ehb.be', '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', 'STUDENT'),
+('Bilal', 'Hlal', 'sofie.achaoui@student.ehb.be', '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', 'STUDENT'),
 ('Bart', 'Bellemans', 'bart.bellemans@docent.ehb.be', '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', 'DOCENT')
 
 ON DUPLICATE KEY UPDATE
@@ -70,6 +71,15 @@ INSERT INTO student_profiles (user_id, studentnummer, opleiding)
 SELECT id, 'STU006', 'Toegepaste Informatica'
 FROM users
 WHERE email = 'imane.achaoui@student.ehb.be'
+ON DUPLICATE KEY UPDATE
+    studentnummer = VALUES(studentnummer),
+    opleiding = VALUES(opleiding);
+
+
+INSERT INTO student_profiles (user_id, studentnummer, opleiding)
+SELECT id, 'STU007', 'Toegepaste Informatica'
+FROM users
+WHERE email = 'bilal.hlal@student.ehb.be'
 ON DUPLICATE KEY UPDATE
     studentnummer = VALUES(studentnummer),
     opleiding = VALUES(opleiding);
